497c43aa3e12a5e0dd2c378ab7320e1aef7cab06
Luana Raissa Sonaglio <88160281+LuanaRSon@users.noreply.github.com>
2025-05-20 21:15:57 +0000

    adicionando scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Um projeto para praticar 
+
+Um projeto para praticar
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Bora codar!!</h1>
+  return <h1>Bora codar!!</h1>;
 }
 
 export default Home;
